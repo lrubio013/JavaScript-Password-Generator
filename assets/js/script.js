b561d5d0d4d84eb1b0db020f3afcd32d082a09a7
lrubio013 <142952845+lrubio013@users.noreply.github.com>
2023-09-25 23:09:15 -0500

    Added variables for my aphates numbers and special characters

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,8 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
+var alphabet = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+var numbers = ["0","1","2","3","4","5","6","7","8","9"];
+var specialCharacters = ["!","@","#","$","%","^","&","*","(",")","-","_","=","+","[","{","}","]",";",":","'","<",">",",",".","/","?"]
 
 // Write password to the #password input
 function writePassword() {
@@ -12,3 +15,17 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+function generatePassword() {
+  
+  var userChoice = window.prompt("How many characters would you like your password to contain?");
+
+
+
+
+
+return password;
+
+}
+
+generatePassword();
