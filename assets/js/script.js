5904bca4d0cfe84a72f0300c74e59486e9de9d28
lrubio013 <142952845+lrubio013@users.noreply.github.com>
2023-09-26 23:02:30 -0500

    added comments on JS for easy walkthrough of code

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,12 +17,12 @@ function generatePassword() {
   var passwordLength = window.prompt("How many characters would you like your password to contain? Must be between 8-128 characters long");
   var randomPassword = "";
   
-  
+  // makes sure user picks number between 8 and 128
   if (passwordLength < 8 || passwordLength > 128) {
     window.alert("Password must be between 8 and 128 characters long.");
     return;
   }
-  
+  // gives user 4 prompts about password preferences
   else {
     var specialSelection = confirm("Click OK to confirm special characters");
     var numericSelection = confirm("Click OK to confirm numbers");
@@ -34,15 +34,16 @@ function generatePassword() {
   if (uppercaseSelection) { multiSelect += multiSelect.concat(alphabet)};
   if (lowercaseSelection) { multiSelect += multiSelect.concat(alphabetLower)};
 
+  // gives user alert if didnt confirm atleast one of four prompts
   if (!specialSelection && !numericSelection && !uppercaseSelection && !lowercaseSelection) {
     alert("You must confirm atleast one option.")
     return;
   }
-
+  //randomizes password after user goes through all prompts
   for (var i = 0; i < passwordLength; i++) {
     randomPassword += multiSelect[Math.floor(Math.random() * multiSelect.length)]
 
     
   }
-   writePassword(randomPassword);
+   writePassword(randomPassword); //Displays random password on textbox for user to see
  }
