5cee4cf800747093cbc267f05f3d72dc45db9fbd
lrubio013 <142952845+lrubio013@users.noreply.github.com>
2023-09-26 22:33:28 -0500

    updated my arays in my variables

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,9 +1,9 @@
 // Assignment Code
 var generateBtn = document.querySelector("#generate");
-var alphabet = ["A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"];
-var alphabetLower = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
-var numbers = ["0", "1", "2", "3", "4", "5", "6", "7", "8", "9"];
-var specialCharacters = ["!", "@", "#", "$", "%", "^", "&", "*", "(", ")", "-", "_", "=", "+", "[", "{", "}", "]", ";", ":", "'", "<", ">", ",", ".", "/", "?"];
+var alphabet = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"];
+var alphabetLower = ["abcdefghijklmnopqrstuvwxyz"];
+var numbers = ["0123456789"];
+var specialCharacters = ["!@#$%^&*()-_=+[{}];:'<>,./?"];
 var multiSelect = [];
 // Write password to the #password input
 function writePassword(password) {
